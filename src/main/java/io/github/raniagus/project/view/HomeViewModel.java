1bbccd3336cc24a5a33df915f15eaaa4aea7d9af
Agustin Ranieri <aguseranieri@gmail.com>
2022-10-14 17:52:47 -0300

    fix script load

--- src/main/java/io/github/raniagus/project/view/HomeViewModel.java
+++ src/main/java/io/github/raniagus/project/view/HomeViewModel.java
@@ -2,13 +2,19 @@ package io.github.raniagus.project.view;
 
 public class HomeViewModel implements ViewModel {
   public String username;
+  public boolean isAdmin;
 
-  public HomeViewModel(String username) {
+  public HomeViewModel(String username, boolean isAdmin) {
     this.username = username;
+    this.isAdmin = isAdmin;
   }
 
   public String getUsername() {
     return username;
+  }
+
+  public boolean isAdmin() {
+    return isAdmin;
   }
 
   @Override
