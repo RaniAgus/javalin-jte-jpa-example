018017a1b396f80ec7d1697796566caaae291c02
Agustin Ranieri <aguseranieri@gmail.com>
2023-01-31 21:18:21 -0300

    validate database schema in prod

--- src/main/java/io/github/raniagus/example/config/ProdConfiguration.java
+++ src/main/java/io/github/raniagus/example/config/ProdConfiguration.java
@@ -16,7 +16,8 @@ public class ProdConfiguration extends Configuration {
     WithSimplePersistenceUnit.configure(properties -> properties
         .set("hibernate.connection.url", System.getenv("DB_URL"))
         .set("hibernate.connection.username", System.getenv("DB_USERNAME"))
-        .set("hibernate.connection.password", System.getenv("DB_PASSWORD")));
+        .set("hibernate.connection.password", System.getenv("DB_PASSWORD"))
+        .set("hibernate.hbm2ddl.auto", "validate"));
   }
 
   @Override
