b9ab634f0735faf9231c5a14e394b4ecb522ecc2
Agustin Ranieri <aguseranieri@gmail.com>
2023-02-15 12:34:02 -0300

    add injector holder docs

--- src/main/java/io/github/raniagus/example/config/InjectorHolder.java
+++ src/main/java/io/github/raniagus/example/config/InjectorHolder.java
@@ -3,6 +3,12 @@ package io.github.raniagus.example.config;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
+/**
+ * Esta clase es un service locator: nos permite inyectar dependencias en
+ * otras clases a través de una única instancia de {@link Injector}.
+ * Nos será útil en caso de necesitar inyectar alguna dependencia en alguna
+ * clase instanciable por Hibernate, o en el main de la aplicación.
+ */
 public enum InjectorHolder {
   INSTANCE;
 
